13cab9bae81498fd9f54db89c9265b10cb892bc7
Priyanka Khatri <preeyankakc.07@gmail.com>
2026-08-29 12:48:19 +0545

    feat: mongodb connected remotely

--- backend/app/config/database.py
+++ backend/app/config/database.py
@@ -1,16 +1,21 @@
 import os
+import urllib.parse
 
 from dotenv import load_dotenv
 from pymongo import AsyncMongoClient
+from pymongo.server_api import ServerApi
 
 
 load_dotenv()
 
-MONGODB_URI = os.getenv("MONGODB_URI")
+DB_USERNAME = urllib.parse.quote_plus(os.getenv("DB_USERNAME", ""))
+DB_PASSWORD = urllib.parse.quote_plus(os.getenv("DB_PASSWORD", ""))
 DATABASE_NAME = os.getenv("DATABASE_NAME", "dynamic-survey-platform")
 
+MONGODB_URI = f"mongodb+srv://{DB_USERNAME}:{DB_PASSWORD}@cluster0.ifsjzlu.mongodb.net/?appName=Cluster0"
 
-client = AsyncMongoClient(MONGODB_URI)
+# Create a new client and connect to the server using the async driver
+client = AsyncMongoClient(MONGODB_URI, server_api=ServerApi('1'))
 
 db = client[DATABASE_NAME]
 
